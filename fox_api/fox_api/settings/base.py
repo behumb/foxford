da547206b1f4a63868909acd1a1ad7a5d22d0b8c
behumb <behumbleplez@gmail.com>
2023-03-06 13:42:07 +0300

    Create dump data file and requirements for project

--- fox_api/fox_api/settings/base.py
+++ fox_api/fox_api/settings/base.py
@@ -101,3 +101,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+FILE_UPLOAD_TEMP_DIR = os.path.join(BASE_DIR, 'dumped_data/')
